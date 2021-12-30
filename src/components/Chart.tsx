8853470703883e305a0707814700777e26b8d6b8
이찬호 <chanholee275@gmail.com>
2021-12-30 12:53:36 +0900

    feat: apexchart를 wrapping 할 컴포넌트 작성

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import ApexCharts, { ApexOptions } from 'apexcharts';
 
 // Wrapper for Chart
-const Chart = () => {
-    return <div></div>;
+const Chart = ({ options }: { options: ApexOptions }) => {
+    const chartRef = React.useRef<HTMLDivElement>(null);
+    React.useEffect(() => {
+        const chart = new ApexCharts(chartRef.current, options);
+        chart.render();
+    }, []);
+    return <div ref={chartRef}></div>;
 };
 
 export default Chart;
