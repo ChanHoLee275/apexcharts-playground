26e1aaf516b809d57ef40742719ab276bf2f38b9
이찬호 <chanholee275@gmail.com>
2022-01-08 17:56:56 +0900

    fix: 차트의 값이 업데이트 될 때, 차트를 새로 그리도록 코드 수정

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -4,10 +4,17 @@ import ApexCharts, { ApexOptions } from 'apexcharts';
 // Wrapper for Chart
 const Chart = ({ options }: { options: ApexOptions }) => {
     const chartRef = React.useRef<HTMLDivElement>(null);
+    const [chartObject, setChartObject] = React.useState<ApexCharts | null>(
+        null
+    );
     React.useEffect(() => {
         const chart = new ApexCharts(chartRef.current, options);
+        setChartObject(chart);
         chart.render();
     }, []);
+
+    if (chartObject !== null) chartObject.updateOptions(options);
+
     return <div ref={chartRef}></div>;
 };
 
