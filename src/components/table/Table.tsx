33bc451ce553ac1a33e33d0a4784e26684526deb
이찬호 <chanholee275@gmail.com>
2022-01-10 16:18:24 +0900

    feat: 테이블 구성을 위한 파일 생성

---
+++ src/components/table/Table.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Table = () => {
+    const [figures, setFigures] = React.useState<string[]>([]);
+    const [data, setData] = React.useState<string[][]>([]);
+
+    return <table>{}</table>;
+};
