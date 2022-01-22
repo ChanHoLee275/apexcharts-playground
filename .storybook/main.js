1149474add2e701a6b04ed83228b393e8aa00ebc
이찬호 <chanholee275@gmail.com>
2022-01-22 16:12:39 +0900

    chore: storybook path alias 적용

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,14 +1,19 @@
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app"
-  ],
-  "core": {
-    "builder": "webpack5"
-  }
-}
+    stories: [
+        '../src/**/*.stories.mdx',
+        '../src/**/*.stories.@(js|jsx|ts|tsx)',
+    ],
+    addons: [
+        '@storybook/addon-links',
+        '@storybook/addon-essentials',
+        '@storybook/preset-create-react-app',
+    ],
+    core: {
+        builder: 'webpack5',
+    },
+    webpackFinal: async (config) => {
+        config.resolve.plugins.push(new TsconfigPathsPlugin({}));
+        return config;
+    },
+};
