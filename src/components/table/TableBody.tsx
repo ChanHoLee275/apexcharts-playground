dcbd0cf3dc9a43b3b35af5467ccc6869a7701090
이찬호 <chanholee275@gmail.com>
2022-01-22 15:56:15 +0900

    feat: 입력을 받을 테이블의 바디 컴포넌트 작성

--- src/components/table/TableBody.tsx
+++ src/components/table/TableBody.tsx
@@ -1,1 +1,20 @@
 import React from 'react';
+import { TableBodyProps } from '../../interface/table/TableBodyProps';
+
+const TableBody = ({ data }: TableBodyProps) => {
+    return (
+        <tbody>
+            {data.map((el, idx) => {
+                return (
+                    <tr key={idx}>
+                        {el.map((data, idx2) => (
+                            <td key={idx2}>{data}</td>
+                        ))}
+                    </tr>
+                );
+            })}
+        </tbody>
+    );
+};
+
+export default TableBody;
